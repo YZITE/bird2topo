6b1aaa7a19cbab6335db5e0e4201769ce0d22f4c
Erik Zscheile <zseri.devel@ytrizja.de>
2020-04-28 17:00:12 +0200

    don't spawn one thread per client

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,73 @@
 use crossbeam_channel as chan;
 use std::thread::spawn;
 use std::time::{Duration, Instant};
+use tracing::debug;
 
 mod gather;
-mod handler;
 mod parser;
+mod tokens;
 
 static OSPF_PROTOS: &[&str] = &["ytrizja", "ytrizja_v6"];
+
+use crate::tokens::{TokenGuard, TokenValue, Tokens};
+
+enum Handler {
+    PreOpen {
+        ws_sender: ws::Sender,
+        tokens: Tokens<ws::Sender>,
+    },
+    PreRunning,
+    Running {
+        tg: TokenGuard<ws::Sender>,
+    },
+    Closed,
+}
+
+impl ws::Handler for Handler {
+    fn on_open(&mut self, shake: ws::Handshake) -> ws::Result<()> {
+        match std::mem::replace(self, Handler::PreRunning) {
+            Handler::PreOpen { ws_sender, tokens } => {
+                if let Some(addr) = shake.remote_addr()? {
+                    debug!("Connection with {} now open", addr);
+                }
+                *self = Handler::Running {
+                    tg: tokens
+                        .try_acquire(ws_sender)
+                        .expect("unable to acquire token"),
+                };
+                Ok(())
+            }
+            _ => panic!("tried to open already opened Handler object"),
+        }
+    }
+
+    fn on_close(&mut self, code: ws::CloseCode, reason: &str) {
+        debug!("Connection closing due to ({:?}) {}", code, reason);
+        if let Handler::Running { tg } = std::mem::replace(self, Handler::Closed) {
+            std::mem::drop(tg);
+        } else {
+            panic!("tried to close not running Handler object");
+        }
+    }
+}
 
 fn main() {
     tracing_subscriber::fmt::init();
 
-    let (s_upd, r_upd) = chan::bounded(0);
-    let (s_thinf, r_thinf) = chan::bounded(1);
+    let (s_tkinf, r_tkinf) = chan::unbounded();
+    let tokens = Tokens::new(s_tkinf);
 
     spawn(move || {
+        use rand::prelude::*;
         let mut prev_hash = None;
+        let mut senders: std::collections::BTreeMap<TokenValue, ws::Sender> = Default::default();
+        let mut rng = rand::thread_rng();
         loop {
+            let sel_start = Instant::now();
+            let mut timeout = chan::after(Duration::from_secs(10));
+
             // update data regulary
+            let mut got_update = false;
             if let Some(dath) = gather::gather(OSPF_PROTOS) {
                 use std::hash::{Hash, Hasher};
                 let mut hasher = std::collections::hash_map::DefaultHasher::new();
@@ -25,24 +75,43 @@ fn main() {
                 let new_hash = hasher.finish();
                 // only report update if hash mismatches
                 if std::mem::replace(&mut prev_hash, Some(new_hash)) != Some(new_hash) {
-                    if s_upd.send_timeout(dath, Duration::from_secs(10)).is_err() {
-                        // got timeout
+                    if senders.is_empty() {
                         // wait for new Handler to appear
-                        let _ = r_thinf.recv();
-                        continue;
+                        timeout = chan::never();
+                    } else {
+                        let mut ids: Vec<TokenValue> = senders.keys().copied().collect();
+                        ids.shuffle(&mut rng);
+                        // every websocket client gets the update
+                        senders
+                            .get_mut(&ids.pop().unwrap())
+                            .unwrap()
+                            .broadcast(dath)
+                            .expect("ws_sender.broadcast failed");
+                        got_update = true;
                     }
+                }
+            }
+            if !senders.is_empty() && !got_update {
+                // ping everybody
+                for i in senders.values_mut() {
+                    i.ping(Vec::new()).expect("ws_sender.ping failed");
                 }
             }
 
             // don't loop too fast
-            let sel_start = Instant::now();
-            let timeout = chan::after(Duration::from_secs(10));
             while sel_start.elapsed() < Duration::from_millis(100) {
+                use crate::tokens::TokenUpdate;
                 chan::select! {
-                    recv(r_thinf) -> thinf => {
-                        match thinf {
+                    recv(r_tkinf) -> tkinf => {
+                        match tkinf {
                             Err(_) => break,
-                            Ok(()) => prev_hash = None,
+                            Ok(TokenUpdate::Acquire(t, s)) => {
+                                prev_hash = None;
+                                senders.insert(t, s);
+                            },
+                            Ok(TokenUpdate::Release(t)) => {
+                                senders.remove(&t);
+                            },
                         }
                     },
                     recv(timeout) -> _ => {},
@@ -51,9 +120,8 @@ fn main() {
         }
     });
 
-    ws::listen("127.0.0.1:8942", |ws_sender| handler::Handler::PreOpen {
-        thinf_chan: s_thinf.clone(),
-        upd_chan: r_upd.clone(),
+    ws::listen("127.0.0.1:8942", |ws_sender| Handler::PreOpen {
+        tokens: tokens.clone(),
         ws_sender,
     })
     .expect("unable to launch WebSocket listener");
---
+++ src/tokens.rs
@@ -0,0 +1,74 @@
+use bit_set::BitSet;
+use crossbeam_channel as chan;
+use std::sync::{Arc, Mutex};
+use tracing::{debug, error};
+
+pub type TokenValue = usize;
+
+#[derive(Debug)]
+pub enum TokenUpdate<T> {
+    Acquire(TokenValue, T),
+    Release(TokenValue),
+}
+
+struct TokensInner<T> {
+    data: Mutex<BitSet<u64>>,
+    evc: chan::Sender<TokenUpdate<T>>,
+}
+
+#[derive(Clone)]
+pub struct Tokens<T>(Arc<TokensInner<T>>);
+
+pub struct TokenGuard<T> {
+    parent: Arc<TokensInner<T>>,
+    tokval: TokenValue,
+}
+
+impl<T> Drop for TokenGuard<T> {
+    fn drop(&mut self) {
+        let tv = self.tokval;
+        let mut is_success = false;
+        if let Ok(mut tks) = self.parent.data.lock() {
+            if tks.insert(tv) {
+                is_success = true;
+            } else {
+                error!("unable to return token {}", tv);
+            }
+        } else {
+            error!("tokens are poisoned");
+        }
+        if is_success {
+            let _ = self.parent.evc.send(TokenUpdate::Release(tv));
+            debug!("released token {}", tv);
+        }
+    }
+}
+
+impl<T> Tokens<T> {
+    pub fn new(evc: chan::Sender<TokenUpdate<T>>) -> Self {
+        let ibs: BitSet<_> = (0..TokenValue::MAX - 1).into_iter().collect();
+        Tokens(Arc::new(TokensInner {
+            data: Mutex::new(ibs),
+            evc,
+        }))
+    }
+
+    pub fn try_acquire(&self, data: T) -> Result<TokenGuard<T>, T> {
+        let tokval = match self.0.data.lock() {
+            Ok(mut tks) => {
+                if let Some(tokval) = tks.iter().next() {
+                    tks.remove(tokval);
+                    tokval
+                } else {
+                    return Err(data);
+                }
+            }
+            Err(_) => return Err(data),
+        };
+        let _ = self.0.evc.send(TokenUpdate::Acquire(tokval, data));
+        Ok(TokenGuard {
+            parent: Arc::clone(&self.0),
+            tokval,
+        })
+    }
+}
